ca4b8144b1e295952f3b8e5217308eeea72209f8
Maria Mitisor <maria.mitisor@gmail.com>
2022-09-17 15:47:05 +0300

    start

---
+++ src/main/java/com/example/demo/bll/Planet.java
@@ -0,0 +1,11 @@
+package com.example.demo.bll;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Planet {
+    private Long Id;
+    private String name;
+
+
+}
---
+++ src/main/java/com/example/demo/bll/Robot.java
@@ -0,0 +1,4 @@
+package com.example.demo.bll;
+
+public class Robot {
+}
